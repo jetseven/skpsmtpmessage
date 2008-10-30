dde5fec2eb58b732d40b57abef6861b9951166c4
ibaird <ibaird@31fc19ee-a5f4-11dd-bf8d-3712e0689f7b>
2008-10-30 15:06:34 +0000

    Initial commit
    git-svn-id: http://skpsmtpmessage.googlecode.com/svn/trunk@3 31fc19ee-a5f4-11dd-bf8d-3712e0689f7b

--- SMTPSender/refresh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Refreshing source from Handshake..."
-
-cp -fv ../Handshake/Classes/SKPSMTPMessage.h Classes/ 
-cp -fv ../Handshake/Classes/SKPSMTPMessage.m Classes/
-cp -fv ../Handshake/Classes/NSStream+SKPSMTPExtensions.h Classes/
-cp -fv ../Handshake/Classes/NSStream+SKPSMTPExtensions.m Classes/
