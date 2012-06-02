d210186e9cf0ad4da624760e24b0ae2b1df6f77d
Steve Brokaw <steve@Bighead.local>
2012-06-02 12:18:34 -0500

    Added support for UTF8 in message data.

--- SMTPLibrary/SKPSMTPMessage.m
+++ SMTPLibrary/SKPSMTPMessage.m
@@ -866,7 +866,7 @@ NSString *kSKPSMTPPartContentTransferEncodingKey = @"kSKPSMTPPartContentTransfer
     [message appendFormat:@"Subject:%@\r\n\r\n",subject];
     [message appendString:separatorString];
     
-    NSData *messageData = [message dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    NSData *messageData = [message dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
     [message release];
     
     NSLog(@"C: %s", [messageData bytes]);
